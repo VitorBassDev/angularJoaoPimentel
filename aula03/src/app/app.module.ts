521e508821113371a008afaaa10eb546ff5cb2c3
Vitor Guedes <vittorbassdev@gmail.com>
2020-10-10 09:32:46 -0300

    Aula 10 OUT 2020 Criação do componente repete Introdução ao If/ELSE Uso do For Utilização do bootstrap

--- aula03/src/app/app.module.ts
+++ aula03/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { RepeteComponent } from './repete/repete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FormularioComponent
+    FormularioComponent,
+    RepeteComponent
   ],
   imports: [
     BrowserModule,
